c9daa09af4c23e2658fd9fb143d8eff1af6f558b
root27 <oguzhaneee@gmail.com>
2024-02-06 12:32:23 +0300

    grpc server

---
+++ build.rs
@@ -0,0 +1,13 @@
+use tonic_build;
+
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::compile_protos("proto/Helloworld.proto")?;
+    Ok(())
+}
+    
+
+
+
+
--- src/server.rs
+++ src/server.rs
@@ -1,30 +1,21 @@
 use tonic::{transport::Server, Request, Response, Status};
-use std::env;
 
 
-pub mod Helloworld {
+pub mod helloworld {
     tonic::include_proto!("Helloworld");
 }
 
 
-
-
-use Helloworld::helloworld_server::{Helloworld, HelloworldServer};
-
-use Helloworld::{HelloRequest, HelloReply};
-
-
 #[derive(Default)]
-
-pub struct MyHelloworld {}
+pub struct MyGreeter {}
 
 #[tonic::async_trait]
 
-impl Helloworld for MyHelloworld {
-    async fn say_hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloReply>, Status> {
+impl helloworld::greeter_server::Greeter for MyGreeter {
+    async fn say_hello(&self, request: Request<helloworld::HelloRequest>) -> Result<Response<helloworld::HelloReply>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = Helloworld::HelloReply {
+        let reply = helloworld::HelloReply {
             message: format!("Hello {}!", request.get_ref().name).into(),
         };
         Ok(Response::new(reply))
@@ -34,18 +25,17 @@ impl Helloworld for MyHelloworld {
 #[tokio::main]
 
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "0.0.0.0:51000";
-
-    let myhelloworld = MyHelloworld::default();
+    let addr = "[::1]:50051".parse().unwrap();
+    let greeter = MyGreeter::default();
 
     Server::builder()
-        .add_service(HelloworldServer::new(myhelloworld))
-        .serve(addr.parse()?)
+        .add_service(helloworld::greeter_server::GreeterServer::new(greeter))
+        .serve(addr)
         .await?;
 
     Ok(())
-
 }
+
 
 
 
