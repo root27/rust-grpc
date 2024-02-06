0caf94cddc2862ffdf88c4d05af65acc4cb40397
root27 <oguzhaneee@gmail.com>
2024-02-06 16:26:34 +0300

    client updated

--- src/client.rs
+++ src/client.rs
@@ -6,20 +6,6 @@ pub mod helloworld {
 }
 
 
-// #[tokio::main]
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let mut client = helloworld::greeter_client::GreeterClient::connect("http://[::1]:50051").await?;
-
-//     let request = Request::new(helloworld::HelloRequest {
-//         name: "Tonic".into(),
-//     });
-
-//     let response = client.say_hello(request).await?;
-
-//     println!("RESPONSE={:?}", response);
-
-//     Ok(())
-// }
 
 
 async fn hello () -> impl Responder {
